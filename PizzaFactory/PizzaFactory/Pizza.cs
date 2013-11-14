7e7b78e60298e064feea804a760562355730b04e
altis10 <altis10@yahoo.com>
2013-11-14 11:48:47 +0200

    Add PizzaFactory

---
+++ PizzaFactory/PizzaFactory/Pizza.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PizzaFactory
+{
+	class Pizza
+	{
+		Pizza orderPizza()
+		{
+			Pizza pizza = new Pizza();
+
+			pizza.prepare();
+			pizza.bake();
+			pizza.cut();
+			pizza.box();
+
+			return pizza;
+		}
+
+		void prepare()
+		{}
+		void bake()
+		{ }
+		void cut()
+		{ }
+		void box()
+		{ }
+	}
+}
